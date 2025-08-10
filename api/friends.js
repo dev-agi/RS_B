9d32a7475377f7f6f0dac36eeb703f1a2c70d126
Agi <116544726+dev-agi@users.noreply.github.com>
2025-08-10 03:11:01 +0300

    Create friends.js

---
+++ api/friends.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const userId = req.query.userId;
+  if (!userId) {
+    return res.status(400).json({ error: 'userId is nil' });
+  }
+
+  const url = `  https://friends.roblox.com/v1/users/${userId}/friends`;
+
+  try {
+    const response = await axios.get(url);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to get api: ' + error.message });
+  }
+};
